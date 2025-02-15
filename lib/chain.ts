8aeac10b685343bfd3bad3aa1353500066670953
Shivam <shivamrai6836@gmail.com>
2025-02-15 10:58:59 +0530

    chain pipeline created

---
+++ lib/chain.ts
@@ -0,0 +1,54 @@
+import { ChatOllama } from "@langchain/ollama";
+import { BufferMemory, ChatMessageHistory } from "langchain/memory";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
+
+import { db } from "./db";
+
+
+
+export const createChain = async (chatId: string, input: string) => {
+    
+    const model = new ChatOllama({
+        // Need to be updated according to the downloaded models 
+        model: "deepseek-r1", 
+        temperature: 0.7,
+    });
+  
+    const prompt = ChatPromptTemplate.fromMessages([
+        ["system", "You are a helpful AI assistant called Eather."],
+        new MessagesPlaceholder("chat_history"),
+        ["human", input],
+    ]);
+
+    const existingMessages = await db.messages
+      .where('chatId')
+      .equals(chatId)
+      .sortBy('timestamp');
+
+    const chatHistory = new ChatMessageHistory();
+  
+    existingMessages.forEach(message => {
+        if (message.role === 'user') {
+            chatHistory.addUserMessage(message.content);
+        } else {
+            chatHistory.addAIMessage(message.content);
+        }
+    });
+
+    const memory = new BufferMemory({
+        memoryKey: "chat_history",
+        returnMessages: true,
+        chatHistory
+    });
+  
+  
+    return RunnableSequence.from([
+        {
+            input: (initialInput) => initialInput.input,
+            chat_history: () => memory.chatHistory.getMessages(),
+        },
+        prompt,
+        model,
+    ]);
+};
